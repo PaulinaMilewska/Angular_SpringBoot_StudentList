c381105ad633ac6db0698cbb372d4c523305ce37
Paulina <milewska.p@wp.pl>
2020-05-13 19:58:22 +0200

    Add postgres properties

--- src/main/java/list/student/StudentController.java
+++ src/main/java/list/student/StudentController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class StudentController {
     @Autowired
     private StudentRepo studentRepo;
